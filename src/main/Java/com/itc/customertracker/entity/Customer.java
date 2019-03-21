fe36ac314a34421dec20eb504a79ea2d55f99abd
ezaika <iegorzaika@gmail.com>
2019-03-21 23:17:18 +0300

    resources folder, css files, view for customers list

--- src/main/Java/com/itc/customertracker/entity/Customer.java
+++ src/main/Java/com/itc/customertracker/entity/Customer.java
@@ -19,4 +19,35 @@ public class Customer {
     @Column(name = "email")
     private String eMail;
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String geteMail() {
+        return eMail;
+    }
+
+    public void seteMail(String eMail) {
+        this.eMail = eMail;
+    }
 }
--- src/main/webapp/WEB-INF/view/customer-list.jsp
+++ src/main/webapp/WEB-INF/view/customer-list.jsp
@@ -1,7 +1,26 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+
 <!DOCTYPE html>
 <html>
 <head><title>There are list of customers!</title></head>
 <body>
-<h2>There are list of customers!</h2>
+
+
+<div id="wrapper">
+    <div id="header">
+        <h2>CRM -   Customer Relationship Manager!</h2>
+    </div>
+</div>
+<div id ="content">
+    <table>
+        <tr><th>First name</th><th>Second name</th><th>Email</th></tr>
+        <c:forEach var="tempCustomer" items="${customers}">
+            <tr><td>${tempCustomer.firstName}</td><td>${tempCustomer.lastName}</td><td>${tempCustomer.eMail}</td></tr>
+        </c:forEach>
+    </table>
+
+</div>
+
+
 </body>
 </html>
